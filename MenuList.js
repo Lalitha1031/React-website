f966376b5316de742728bb9f4ff2b5caefd41cda
Lalitha1031 <renu43606@gmail.com>
2024-10-30 23:07:19 +0530

    Add files via upload

---
+++ MenuList.js
@@ -0,0 +1,39 @@
+import Perrironi from '../assest/pepperoni.jpg'
+import OnioPizza from '../assest/pedrotechspecial.jpg'
+import Panner from '../assest/expensive.jpg'
+import CO from '../assest/vegan.jpg'
+import  marg from '../assest/margherita.jpg'
+import loaded from '../assest/pineapple.jpg'
+//we can change the information from this file easily
+export const  MenuList=[
+    {
+        name:"Perrironi Pizza",
+        image:Perrironi,
+        price:200,
+    },
+    {
+        name:"Pyasi Pizza",
+        image:OnioPizza,
+        price:150,
+    },
+    {
+        name:"Panner Pizza",
+        image:Panner,
+        price:160,
+    },
+    {
+        name:"Corn&Onion",
+        image:CO,
+        price:120,
+    },
+    {
+        name:"Margiritte",
+        image:marg,
+        price:100,
+    },
+    {
+        name:"Veg Farm-house",
+        image:loaded,
+        price:180,
+    },
+];
